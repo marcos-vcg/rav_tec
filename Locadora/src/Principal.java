28d0c56fadcab9a28c15e93d840f3ccf10d6e98d
marcos_vcg <marcos.vcg.ads@gmail.com>
2020-09-28 20:14:10 -0300

    commit

---
+++ Locadora/src/Principal.java
@@ -0,0 +1,48 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JToolBar bar_cadastro = new JToolBar();
+		bar_cadastro.setBounds(0, 0, 87, 16);
+		contentPane.add(bar_cadastro);
+		
+		JToolBar bar_locacao = new JToolBar();
+		bar_locacao.setBounds(95, 0, 87, 16);
+		contentPane.add(bar_locacao);
+	}
+}
