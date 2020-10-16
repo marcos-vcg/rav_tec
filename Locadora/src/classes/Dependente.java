f2ffd5ad8e61cac5a0f70a7764347e537d098827
marcos_vcg <marcos.vcg.ads@gmail.com>
2020-10-16 18:58:18 -0300

    addContadorSerializado

--- Locadora/src/classes/Dependente.java
+++ Locadora/src/classes/Dependente.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class Dependente implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String nome;
 	private Grau grau;
 	private Integer id;
